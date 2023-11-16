b13e94ae40ed91809276bb2dfbf4db58f8dd1d01
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2023-11-16 22:25:16 +0300

    feat: add header, icon and menu components

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import iconsSprite from '../../assets/images/icons-sprite.svg'
+
+type IconPropsType = {
+  iconId: string,
+  width?: string,
+  height?: string,
+  viewBox?: string,
+}
+
+export const Icon = (props: IconPropsType) => {
+  return (
+    <svg width={props.width || "66"} height={props.height || "66"} viewBox={props.viewBox || "0 0 66 66"} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <use xlinkHref={ `${iconsSprite}#${props.iconId}` }/> 
+    </svg>
+  );
+};
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { Menu } from "../../components/menu/Menu";
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <Menu />
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  background-color: green;
+`;

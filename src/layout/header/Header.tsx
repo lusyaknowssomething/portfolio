d459d596f13694ccffa64e37a2b39f43d2f36b7b
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2023-11-18 22:36:32 +0300

    feat: add styles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { Menu } from "../../components/menu/Menu";
+import { Container } from "../../components/Container";
+import { Button } from "../../components/Button";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 const menuItems = [
   {name: 'Home', link: '', id: '1'}, 
@@ -13,11 +16,22 @@ const menuItems = [
 export const Header = () => {
   return (
     <StyledHeader>
-      <Menu items={menuItems} />
+      <Container>
+        <FlexWrapper justify={"space-between"} align={"center"}>
+        <Menu items={menuItems} />
+        <Button>Projects Here</Button>
+        </FlexWrapper>
+      </Container>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
-  background-color: green;
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
+  background-color: #ffffffcd;
 `;
---
+++ src/styles/Theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    accent: '#FD5956',
+    font: '#000000',
+    fontButton: '#FFFEFD',
+  }
+}

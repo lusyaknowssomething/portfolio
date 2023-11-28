c15a7ca4fb81091145ca84b3d83ba9ed5a181516
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2023-11-28 06:00:06 +0300

    style: add styles for contacts block

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -5,41 +5,73 @@ import { Button } from "../../../components/Button";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import imageForContact from "../../../assets/images/photos/For-footer.png";
 import { Dot } from "../../../components/Dot";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 export const Contacts = () => {
   return (
     <StyledContacts>
-      <FlexWrapper align="center" justify="space-between">
-        <Image src={imageForContact} />
-        <FlexWrapper direction="column">
-        <SectionTitle>Contacts<Dot>.</Dot></SectionTitle>
-        <Text>Please fill out the form on this section to contact with me or send me an email. I will answer you as soon as I receive your message.</Text>
-        <StyledForm>
-          <Field placeholder={"name"} />
-          <Field placeholder={"email"} />
-          <Field placeholder={"message"} as={"textarea"} />
-          <Button type={"submit"}>Send message</Button>
-        </StyledForm>
+      <Container>
+        <FlexWrapper align="center" justify="space-between">
+          <Image src={imageForContact} />
+          <FlexWrapper direction="column">
+            <SectionTitle>
+              Contacts<Dot>.</Dot>
+            </SectionTitle>
+            <Text>
+              Please fill out the form on this section to contact with me or
+              send me an email. I will answer you as soon as I receive your
+              message.
+            </Text>
+            <StyledForm>
+              <Field placeholder={"Name"} />
+              <Field placeholder={"Email"} />
+              <Field placeholder={"Message"} as={"textarea"} />
+              <Button type={"submit"} bgcolor={theme.colors.accent}>
+                Send message
+              </Button>
+            </StyledForm>
+          </FlexWrapper>
         </FlexWrapper>
-        
-      </FlexWrapper>
+      </Container>
     </StyledContacts>
   );
 };
 
-const StyledContacts = styled.section`
-  min-height: 50vh;
-  background-color: lightyellow;
-`;
+const StyledContacts = styled.section``;
 
 const StyledForm = styled.form`
-  max-width: 500px;
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 16px;
+  align-items: center;
+
+  textarea {
+    resize: none;
+    height: 220px;
+    width: 100%;
+  }
 `;
-const Field = styled.input``;
+
+const Field = styled.input`
+  width: 100%;
+  border: 1px solid ${theme.colors.formBorder};
+  font-family: Poppins;
+  padding: 7px 15px;
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.6px;
+
+  &::placeholder {
+  color: ${theme.colors.formFont};
+  }
+
+  &:focus-visible {
+    border: 1px solid ${theme.colors.accent};
+    outline: none;
+  }
+`;
 
 const Image = styled.img`
   object-fit: cover;
@@ -47,4 +79,10 @@ const Image = styled.img`
   max-width: 557px;
 `;
 
-const Text = styled.p``;
+const Text = styled.p`
+  text-align: center;
+  font-size: 20px;
+  font-weight: 400;
+  margin-bottom: 103px;
+  margin-top: -60px;
+`;
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -3,5 +3,7 @@ export const theme = {
     accent: '#FD5956',
     font: '#000000',
     fontButton: '#FFFEFD',
+    formBorder: '#868686',
+    formFont: '#495057',
   }
 }

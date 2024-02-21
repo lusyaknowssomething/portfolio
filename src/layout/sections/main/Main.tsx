20fbf6e7790b86ee8c74aafc7732fdf326328aa6
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2024-02-21 19:33:17 +0300

    refactor: Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,108 +1,29 @@
 import React from "react";
-import styled from "styled-components";
 import photoMain from "../../../assets/images/photos/For-main.png";
-import Graphic from "../../../assets/images/graphics/Vector_dotted.svg";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { SocialIcons } from "../../../components/socialIcons/SocialIcons";
 import { Button } from "../../../components/Button";
 import { Container } from "../../../components/Container";
 import { theme } from "../../../styles/Theme";
-import { font } from "../../../styles/Common";
+import { S } from "./Main_Styles";
 
-export const Main = () => {
+export const Main: React.FC = () => {
   return (
-    <StyledMain>
+    <S.StyledMain>
       <Container>
         <FlexWrapper align="center" justify="space-between" wrap="wrap">
           <div>
-            <SmallText>Hello! I’m</SmallText>
-            <Name>Khatanzeyskaya Lyudmila</Name>
-            <MainTitle>Frontend <br/><Mask>Developer</Mask></MainTitle>
+            <S.SmallText>Hello! I’m</S.SmallText>
+            <S.Name>Khatanzeyskaya Lyudmila</S.Name>
+            <S.MainTitle>Frontend <br/><S.Mask>Developer</S.Mask></S.MainTitle>
             <SocialIcons />
             <Button bgcolor={theme.colors.accent}>HIRE ME</Button>
           </div>
-          <PhotoWrapper>
-            <Photo src={photoMain} alt="lyudmila" />
-          </PhotoWrapper>
+          <S.PhotoWrapper>
+            <S.Photo src={photoMain} alt="lyudmila" />
+          </S.PhotoWrapper>
         </FlexWrapper>
       </Container>
-    </StyledMain>
+    </S.StyledMain>
   );
 };
-
-const StyledMain = styled.section`
-  display: flex;
-  background: no-repeat url(${Graphic}) bottom left 30%;
-  background-size: 661px;
-
-  
-`;
-
-const MainTitle = styled.h1`
-  ${font({weight: 600, Fmin: 50, Fmax: 100})}
-  padding-top: 15px;  
-
-  @media ${theme.media.mobile} {
-    padding-top: 35px;
-  }
-`;
-
-const PhotoWrapper = styled.div`
-  max-width: 483px;
-`
-
-const Photo = styled.img`
-  width: 100%;
-  object-fit: cover;
-`;
-
-const SmallText = styled.span`
-  display: block;
-  font-size: 16px;
-  font-weight: 600;
-
-  @media ${theme.media.tablet} {
-    padding-top: 100px;
-  }
-`;
-
-const Name = styled.span`
-  ${font({weight: 600, color: theme.colors.accent, Fmin: 22, Fmax: 28})}
-  display: block;
-  padding-top: 20px;
-
-  @media ${theme.media.mobile} {
-    padding-top: 15px;
-  }
-`;
-
-const Mask = styled.span`
-  display: inline-block;
-  padding-left: 69px;
-  position: relative;
-  padding-bottom: 72px;
-
-  @media ${theme.media.mobile} {
-    padding-left: 20px;
-    padding-bottom: 50px;
-  }
-
-  &::after {
-    content: "";
-    background-color: ${theme.colors.accent};
-    width: 17px;
-    height: 17px;
-    position: absolute;
-    z-index: 1;
-    right: -30px;
-    bottom: 80px;
-    border-radius: 50%;
-
-    @media ${theme.media.mobile} {
-      width: 10px;
-      height: 10px;
-      right: -10px;
-      bottom: 60px;
-  }
-  }
-`;
---
+++ src/layout/sections/main/Main_Styles.ts
@@ -0,0 +1,92 @@
+import { theme } from './../../../styles/Theme';
+import styled from "styled-components";
+import Graphic from "../../../assets/images/graphics/Vector_dotted.svg";
+import { font } from "../../../styles/Common";
+
+
+const StyledMain = styled.section`
+  display: flex;
+  background: no-repeat url(${Graphic}) bottom left 30%;
+  background-size: 661px;
+
+  
+`;
+
+const MainTitle = styled.h1`
+  ${font({weight: 600, Fmin: 50, Fmax: 100})}
+  padding-top: 15px;  
+
+  @media ${theme.media.mobile} {
+    padding-top: 35px;
+  }
+`;
+
+const PhotoWrapper = styled.div`
+  max-width: 483px;
+`
+
+const Photo = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const SmallText = styled.span`
+  display: block;
+  font-size: 16px;
+  font-weight: 600;
+
+  @media ${theme.media.tablet} {
+    padding-top: 100px;
+  }
+`;
+
+const Name = styled.span`
+  ${font({weight: 600, color: theme.colors.accent, Fmin: 22, Fmax: 28})}
+  display: block;
+  padding-top: 20px;
+
+  @media ${theme.media.mobile} {
+    padding-top: 15px;
+  }
+`;
+
+const Mask = styled.span`
+  display: inline-block;
+  padding-left: 69px;
+  position: relative;
+  padding-bottom: 72px;
+
+  @media ${theme.media.mobile} {
+    padding-left: 20px;
+    padding-bottom: 50px;
+  }
+
+  &::after {
+    content: "";
+    background-color: ${theme.colors.accent};
+    width: 17px;
+    height: 17px;
+    position: absolute;
+    z-index: 1;
+    right: -30px;
+    bottom: 80px;
+    border-radius: 50%;
+
+    @media ${theme.media.mobile} {
+      width: 10px;
+      height: 10px;
+      right: -10px;
+      bottom: 60px;
+  }
+  }
+`;
+
+export const S = {
+  StyledMain,
+  MainTitle,
+  PhotoWrapper,
+  Photo,
+  SmallText,
+  Name,
+  Mask
+}
